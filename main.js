2a44c2a8b01249d71fe829aed9ff02c567e73378
RoyaleHighrblx <96347364+RoyaleHighrblx@users.noreply.github.com>
2022-07-15 22:47:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,13 +10,14 @@ function setup(){
     canvas.position(560, 150);
 
     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
 
 function modelLoaded(){
     console.log('PoseNet is Initialized!');
 }
 
-function gotPoases(results){
+function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
